d37e7301ccdce1e41abb61b8942c1e945c32d021
k <devs079@yahoo.co.jp>
2019-11-29 23:44:37 +0900

    update rules

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import * as React from 'react';
 import Header from './components/Header';
 import Microphone from './components/Microphone';
 
-const App: React.FC<{}> = (): JSX.Element => {
+export default function App() {
   return (
     <div>
       <Header />
       <Microphone />
     </div>
   );
-};
-
-export default App;
+}
